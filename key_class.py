55cce72170d10e51587a0cf2b187aed10c909661
gonsr <gon.srugo@nitzanim.tech>
2025-12-09 15:30:26 +0200

    add key class

--- key_class.py
+++ key_class.py
@@ -1,7 +1,11 @@
 import arcade
 
 class Key(arcade.Sprite):
-    def __init__(self,x,y):
+    def super().__init__(self,x,y)
         self.center_x=x
         self.center_y=y
-        self.texture=
+        self.texture=arcade.make_soft_square_texture(
+            size,                 # Width and height of the square
+            arcade.color.GOLD,    # Color
+            outer_margin=255      # Margin parameter (usually left at 255 for solid)
+        )
