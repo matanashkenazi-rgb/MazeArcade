e4211c55cae31fc4b5eebeb55ac86f8f0fdc3d1c
Matan <mataniwani1999@gmail.com>
2025-12-09 15:55:21 +0200

    Fixed maze_Game

--- class_maze_game.py
+++ class_maze_game.py
@@ -26,6 +26,26 @@ class Maze_game(arcade.View):
         arcade.set_background_color(self.back_ground_color)
 
         self.wall_list.draw()
+        self.key_list.draw()
+        self.exit_list.draw()
+        self.player_list.draw()
+
+        if self.player and self.player.has_key:
+            key_text = "Key: Collected!"
+        else:
+            key_text = "Key: Not collected"
+
+        arcade.draw_text(
+            key_text,
+            start_x=10,  # מרחק מהצד השמאלי
+            start_y=self.window.height - 20,  # למעלה
+            color=arcade.color.WHITE,
+            font_size=14
+        )
+
+
+
+
 
 
 
