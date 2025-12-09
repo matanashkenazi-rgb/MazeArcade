2d31c59dbe8a024f897f99fb520e329a52cbb889
Matan <mataniwani1999@gmail.com>
2025-12-09 16:01:57 +0200

    Added on_draw

--- class_maze_game.py
+++ class_maze_game.py
@@ -1,4 +1,5 @@
 import arcade
+from player import Player
 
 class Maze_game(arcade.View):
     def __init__(self):
@@ -8,12 +9,13 @@ class Maze_game(arcade.View):
         self.exit_list=arcade.SpriteList()
         self.player_list=arcade.SpriteList()
         self.player=arcade.SpriteList()
+        self.player = None
         self.game_won=False
         self.back_ground_color=arcade.color.BLACK
 
     def setup(self):
         pass
-
+git ad
 
 
 
@@ -30,18 +32,24 @@ class Maze_game(arcade.View):
         self.exit_list.draw()
         self.player_list.draw()
 
-        if self.player and self.player.has_key:
-            key_text = "Key: Collected!"
-        else:
-            key_text = "Key: Not collected"
-
+        key_text = "Key: Collected!" if self.player.has_key else "Key: Not Collected"
         arcade.draw_text(
             key_text,
-            start_x=10,  # מרחק מהצד השמאלי
-            start_y=self.window.height - 20,  # למעלה
-            color=arcade.color.WHITE,
-            font_size=14
+            10,
+            self.window.height - 20,
+            arcade.color.WHITE,
+            14
         )
+        if self.game_won:
+            arcade.draw_text(
+                "YOU WIN!",
+                self.window.width // 2,
+                self.window.height // 2,
+                arcade.color.YELLOW,
+                40,
+                anchor_x="center",
+                anchor_y="center"
+            )
 
 
 
