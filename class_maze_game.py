886718f211a575f7f3b5e28f008d657fcdca7a93
Matan <mataniwani1999@gmail.com>
2025-12-09 15:54:27 +0200

    Fixed maze_Game

--- class_maze_game.py
+++ class_maze_game.py
@@ -1,13 +1,31 @@
 import arcade
 
-class Maze_game(arcade.view):
+class Maze_game(arcade.View):
     def __init__(self):
-        self.wall_list=[]
-        self.key_list=[]
-        self.exit_list=[]
-        self.player_list=[]
-        self.player=[]
+        super().__init__()
+        self.wall_list=arcade.SpriteList()
+        self.key_list=arcade.SpriteList()
+        self.exit_list=arcade.SpriteList()
+        self.player_list=arcade.SpriteList()
+        self.player=arcade.SpriteList()
         self.game_won=False
-        self.backgaroundcolor=arcade.color.black
+        self.back_ground_color=arcade.color.BLACK
 
     def setup(self):
+        pass
+
+
+
+
+
+
+
+
+    def on_draw(self):
+        arcade.start_render()
+        arcade.set_background_color(self.back_ground_color)
+
+        self.wall_list.draw()
+
+
+
